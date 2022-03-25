1b2e613fdd1452b749fce7f5dd7e467643cfcd4d
RafaelGuindani <99561131+RafaelGuindani@users.noreply.github.com>
2022-03-25 09:18:49 -0300

    band-name-generator

---
+++ exercicio03 - Nome de Banda.py
@@ -0,0 +1,6 @@
+print(" ","¨¨"*28)
+print('Seja bem vindo ao gerador de nome de bandas')
+cidade = input('Primeiro me conte, em qual cidade nasceu ?\n >: ')
+mascote = input('Agora preciso saber qual o nome do seu animal de estimação?\n >: ')
+print(f'Bom o nome da sua banda poderia ser: {mascote} {cidade}, \nDivirta-se!')
+print("\n","¨¨"*28)
