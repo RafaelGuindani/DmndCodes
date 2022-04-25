0a98f2ac0300fec4ca4699d6fc442b498f0ad8e9
Rafael Guindani <Dmndcode@gmail.com>
2022-04-25 10:40:38 -0300

    Acesso a montanha russa

--- exercicio12 - altura_montanharussa.py
+++ exercicio12 - altura_montanharussa.py
@@ -1,5 +1,6 @@
 print('Seja bem vindo!!!')
 print('Ao Guindaverse WORLLLLD!!!')
+
 print('Para acessar a montanha russa, por gentileza me informe a sua altura'
       '[ex:1.50]')
 altura = float(input('>:'))
@@ -8,5 +9,7 @@ idade = int(input('>:'))
 
 if ((altura < 1.30) and (idade < 10)):
     print('Infelizmente não pode acessar a montanha russa.\nRecomendo brinquedos da ala norte.')
+    print('O ingresso para crianças custa: R$ 5,90')
 else:
+      print('O Ingresso para adultos custa: R$ 11,90')
       print('Divirta-se, não esqueça do cinto de segurança!!!')
