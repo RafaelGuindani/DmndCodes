5bbccd270e99d6eff99c436718923cda5921bdb3
Rafael Guindani <Dmndcode@gmail.com>
2022-05-01 12:00:27 -0300

    Rafael Guindani

--- Atividades Ruby/atividade01 - say_my_name.rb
+++ Atividades Ruby/atividade01 - say_my_name.rb
@@ -1,5 +1,16 @@
 def say_my_name(name)
-    "You are " + name
+    "You are " + name.capitalize
 end
 
+def city(cidade)
+    "You live in #{cidade.capitalize}"
+end
+
+def age(idade)
+    "you have #{idade}".capitalize
+end
+
+"*"*10
 puts say_my_name "Rafael Guindani"
+puts city "curitiba"
+puts age "35 Years"
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# DmndCodes
+br# DmndCodes
 
 Este repositório é relacionado as minhas atividades, exercicios em:
 
+
 > Python.
+<>
 > Ruby.
