0fa212a792e2b2494110c61d12af1b9c8a76be98
Rafael Guindani <Dmndcode@gmail.com>
2022-05-02 18:55:34 -0300

    Atividade 02 - Soma

---
+++ Atividades Ruby/atividade02 - soma.rb
@@ -0,0 +1,10 @@
+
+print('Informe o primeiro número: ')
+n1 = gets.chomp.to_i
+
+print('Informe o segundo número: ')
+n2 = gets.chomp.to_i
+
+soma = n1 + n2
+
+printf("A soma dos dois valores é %d", soma)
