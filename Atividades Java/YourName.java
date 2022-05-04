e467139cf08b9f5e207052b21ded6784d1975f1c
Rafael Guindani <Dmndcode@gmail.com>
2022-05-04 17:44:42 -0300

    Olá + YourName

--- Atividades Java/YourName.java
+++ Atividades Java/YourName.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class YourName {
-}
+import java.util.Scanner;
+
+public class YourName {
+    public static void main(String[] args) {
+        String seunome = "";
+        Scanner digitarnome = new Scanner(System.in);
+
+        System.out.println("Por gentileza, Informe seu nome:\n>: ");
+        seunome = digitarnome.nextLine();
+        System.out.println("Olá "+seunome);
+    }}
