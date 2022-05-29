aa6160639217637f840489d10599bff6309c58e3
Rafael Guindani <Dmndcode@gmail.com>
2022-05-29 12:01:45 -0300

    Calculadora

---
+++ Atividades Java/Main.java
@@ -0,0 +1,16 @@
+public class Mainc {
+
+    public static void main(String[] args) {
+        // CALCULADORA ***
+        System.out.println("Exercicio Calculadora");
+        Calculadora.soma(3,6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7,8);
+        Calculadora.divisao(5,2.5);
+        // MENSAGEM ***
+        System.out.println("Exercício mensagem");
+        Mensagem_.obtermensagem(9);
+        Mensagem_.obtermensagem(14);
+        Mensagem_.obtermensagem(1);
+    }
+}
