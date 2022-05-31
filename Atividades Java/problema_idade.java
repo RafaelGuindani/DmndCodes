b48ab400e350c182de63a2e5208f175a7f2c1e32
Rafael Guindani <Dmndcode@gmail.com>
2022-05-31 18:25:07 -0300

    Atividades Java

--- Atividades Java/problema_idade.java
+++ Atividades Java/problema_idade.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class problema_idade {
+import java.util.Locale;
+import java.util.Scanner;
+
+public class problema_idade {
+    public static void main(String[] args) {
+        //***
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        //***
+        String nome1, nome2;
+        int idade1, idade2;
+        double media;
+        //***
+        System.out.println("Dados da primeira pessoa:");
+        System.out.print("Nome: ");
+        nome1 = sc.nextLine();
+        System.out.print("Idade: ");
+        idade1 = sc.nextInt();
+        //***
+        sc.nextLine();
+        //***
+        System.out.println("Dados da segunda pessoa:");
+        System.out.print("Nome: ");
+        nome2 = sc.nextLine();
+        System.out.print("Idade: ");
+        idade2 = sc.nextInt();
+        //***
+        media = (idade1 + idade2) / 2;
+        //***
+        System.out.println("A idade média de " + nome1 + " e "+ nome2 + " é de " + media + " anos.");
+        //***
+        sc.close();
+    }
 }
