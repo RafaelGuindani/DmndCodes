5dbdee535e441e16ef77ea965f65253f5b9e8473
Rafael Guindani <Dmndcode@gmail.com>
2022-06-04 16:28:21 -0300

    Atividade SQL

--- Atividades Sql/atividade04 - modelando_cafeteria.sql
+++ Atividades Sql/atividade04 - modelando_cafeteria.sql
@@ -1,18 +1,30 @@
 # Inserindo dados - DML
 # *** Tipos de Produtos
 USE db_cafeteria;
+
+# -- Visualizar a planilha
+SELECT * FROM tipos_produtos;
+
 INSERT INTO tipos_produtos (descricao) VALUES ('Cappuccino');
 INSERT INTO tipos_produtos (descricao) VALUES ('Frappuccino');
 INSERT INTO tipos_produtos (descricao) VALUES ('Macchiato');
-INSERT INTO tipos_produtos (descricao) VALUES ('Café latte');
+INSERT INTO tipos_produtos (descricao) VALUES ('Cafe latte');
 INSERT INTO tipos_produtos (descricao) VALUES ('Mocha');
 INSERT INTO tipos_produtos (descricao) VALUES ('NAKED');
 INSERT INTO tipos_produtos (descricao) VALUES ('Brasileirinho');
 
 # ** Adicionar coluna tabela tipos_produtos
-alter table tipos_produtos add column Quantidade varchar(55);
-# -- Visualizar a planilha
-SELECT * FROM tipos_produtos;
+alter table tipos_produtos add column Quantidade int(11) NOT NULL default;
+
+# *** Tipos de Produtos
+# *** Atualizando ID
+UPDATE `db_cafeteria`.`tipos_produtos` SET `Id` = '1' WHERE (`Id` = '15');
+UPDATE `db_cafeteria`.`tipos_produtos` SET `Id` = '2' WHERE (`Id` = '16');
+UPDATE `db_cafeteria`.`tipos_produtos` SET `Id` = '3' WHERE (`Id` = '17');
+UPDATE `db_cafeteria`.`tipos_produtos` SET `Id` = '4' WHERE (`Id` = '18');
+UPDATE `db_cafeteria`.`tipos_produtos` SET `Id` = '5' WHERE (`Id` = '19');
+UPDATE `db_cafeteria`.`tipos_produtos` SET `Id` = '6' WHERE (`Id` = '20');
+UPDATE `db_cafeteria`.`tipos_produtos` SET `Id` = '7' WHERE (`Id` = '21');
 
 # *** Fabricantes
 INSERT INTO fabricantes (fabricante) VALUES ('Melitta');
