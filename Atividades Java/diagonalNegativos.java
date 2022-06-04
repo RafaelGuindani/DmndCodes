0c1f04d426548c7fbc2872f827ac1cdf492a0d90
Rafael Guindani <Dmndcode@gmail.com>
2022-06-04 15:23:26 -0300

    atividades Java

--- Atividades Java/diagonalNegativos.java
+++ Atividades Java/diagonalNegativos.java
@@ -1,2 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class diagonalNegativos {
+    public static void main (String[] args) {
+        //***
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        //***
+        int N, cont;
+        System.out.print("Qual a ordem da MATRIZ ? ");
+        N = sc.nextInt();
+        int [][] mtz = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++){
+                System.out.print("Elemento [" + i + "," + j + "]: ");
+                mtz[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("DIAGONAL PRINCIPAL: ");
+        for (int i = 0; i < N; i++) {
+            System.out.print(mtz[i][i] + " ");
+        }
+        System.out.println("");
+        cont = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++){
+                if (mtz[i][j] < 0) {
+                    cont++;
+                }
+            }
+        }
+        System.out.println("QUANTIDADE DE NEGATIVOS = " + cont);
+        sc.close();
+    }
 }
