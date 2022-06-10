98ca1a586f30ea5f2d705cc5d0c36bb01bdf5fee
Rafael Guindani <Dmndcode@gmail.com>
2022-06-10 19:25:30 -0300

    Python positional Arguments

---
+++ Atividades Python/exercicio35 -PositionalArguments.py
@@ -0,0 +1,11 @@
+a = input ("What is your name?")
+b = input ("Where is your location?")
+
+def newFunction(location, name):
+
+    print(f'Hello {name} ')
+    print(f'How do you do {name}? ')
+    print(f'What is it like in {location} ')
+
+
+newFunction(name=a, location=b)
